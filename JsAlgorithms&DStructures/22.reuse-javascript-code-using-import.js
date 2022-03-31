d09780a6ddb03f561fd87e261fdc60e173576f30
Maby <maby200@gmail.com>
2022-03-31 08:22:04 -0500

    MySolutions

---
+++ JsAlgorithms&DStructures/22.reuse-javascript-code-using-import.js
@@ -0,0 +1,7 @@
+import {uppercaseString,lowercaseString} from './string_functions.js';
+// Only change code above this line
+
+console.log(uppercaseString("hello"));
+
+uppercaseString("hello");
+lowercaseString("WORLD!");
